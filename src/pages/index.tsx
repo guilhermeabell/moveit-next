255dd82e700bec77ca71f00bbaca42a2c01f52e9
guilhermeabel99 <gpeuvis@gmail.com>
2021-02-28 13:19:45 -0300

    countdown add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,15 @@ import { CompleteChallenge } from "../components/CompleteChallenge";
 import styles from '../styles/pages/home.module.css';
 import { Countdown } from "../components/Countdown";
 
+import Head from 'next/head';
+
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+       </Head> 
+
       <ExperienceBar />
       
       <section>
